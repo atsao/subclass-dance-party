bd45d515732634ec041549852ff4eabda93816b2
atsao <andrew.tsao8@gmail.com>
2016-05-09 16:42:15 -0700

    More progress on subclassing sprint - blinking works!

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,28 +1,11 @@
-// var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-//   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
-//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-//   // so we must keep a copy of the old version of this function
-
-//   var oldStep = blinkyDancer.step;
-
-//   blinkyDancer.step = function(){
-//     // call the old version of step at the beginning of any call to this new version of step
-//     oldStep();
-//     // toggle() is a jQuery method to show/hide the <span> tag.
-//     // See http://api.jquery.com/category/effects/ for this and
-//     // other effects you can use on a jQuery-wrapped html tag.
-//     blinkyDancer.$node.toggle();
-//   };
-
-//   return blinkyDancer;
-// };
 var BlinkyDancer = function(top, left, timeBetweenSteps) {
-  Dancer.call(this);
+  Dancer.call(this, top, left, timeBetweenSteps);
   this.top = top;
   this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
-
+  
+  this.step(timeBetweenSteps);
   this.setPosition(top, left);
 }
 
@@ -31,6 +14,7 @@ BlinkyDancer.prototype.constructor = BlinkyDancer;
 
 BlinkyDancer.prototype.step = function(timeBetweenSteps) {
   this.$node.toggle();
+  Dancer.prototype.step.call(this, timeBetweenSteps);
 }
 
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
