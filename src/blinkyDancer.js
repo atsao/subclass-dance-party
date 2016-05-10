9ec330b3514376ec4733bf2ab2930409de89f96a
Andrew Tsao <andrew.tsao8@gmail.com>
2016-05-10 10:05:58 -0700

    Reduced excess dancers

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -2,7 +2,6 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('blinky-dancer');
-  this.callCount = 0;
 }
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
