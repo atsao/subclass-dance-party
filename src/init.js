64c7ac220b54d88bea2868ca6b0fd73ba3309533
atsao <andrew.tsao8@gmail.com>
2016-05-10 17:27:05 -0700

    Added background music and generated base dancers

--- src/init.js
+++ src/init.js
@@ -1,19 +1,7 @@
 $(document).ready(function(){
   window.dancers = [];
 
-function getRandomColor () {
-  var hex = Math.floor(Math.random() * 0xFFFFFF);
-  return "#" + ("000000" + hex.toString(16)).substr(-6);
-}
-  setInterval(function() {
-    $('body').css('background', getRandomColor);
-    $('.spinning-dancer, .blinky-dancer').css({
-      'border-color': getRandomColor
-    });
-    $('.stick-dancer').css({
-      'background-color': getRandomColor
-    });
-  }, 1000);
+  generateDancers();
 
   $(".dancer-button").on("click", function(event){
     /* This function sets up the click handlers for the create-dancer
@@ -77,23 +65,7 @@ function getRandomColor () {
   $('#bash').on('click', function(e) {
     e.preventDefault();
 
-    var dancerFactories = document.getElementsByClassName('dancer-button');
-    var dancerFactoriesFunctions = [];
-    for (var i = 0; i < dancerFactories.length; i++) {
-      dancerFactoriesFunctions.push(dancerFactories[i].getAttribute('data-dancer-maker-function-name'));
-    }
-
-    for (var i = 0; i < 25; i++) {
-      var random = Math.floor(Math.random() * dancerFactoriesFunctions.length);
-      var randomFunc = window[dancerFactoriesFunctions[random]];
-      var dancer = new randomFunc(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
-      Math.random() * 1000
-    );
-    $('body').append(dancer.$node);
-    window.dancers.push(dancer);
-    }
+    generateDancers();
   });
 
   $('#blinky').on('click', function(e) {
@@ -105,6 +77,41 @@ function getRandomColor () {
         $dancer.addClass('blinky');
       }
     })
-  })
+  });
+
+  function generateDancers() {
+    var dancerFactories = document.getElementsByClassName('dancer-button');
+    var dancerFactoriesFunctions = [];
+    for (var i = 0; i < dancerFactories.length; i++) {
+      dancerFactoriesFunctions.push(dancerFactories[i].getAttribute('data-dancer-maker-function-name'));
+    }
+
+    for (var i = 0; i < 10; i++) {
+      var random = Math.floor(Math.random() * dancerFactoriesFunctions.length);
+      var randomFunc = window[dancerFactoriesFunctions[random]];
+      var dancer = new randomFunc(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      Math.random() * 1000
+    );
+    $('body').append(dancer.$node);
+    window.dancers.push(dancer);
+    }
+  }
+
+  function getRandomColor () {
+    var hex = Math.floor(Math.random() * 0xFFFFFF);
+    return "#" + ("000000" + hex.toString(16)).substr(-6);
+  }
+  
+  setInterval(function() {
+    $('body').css('background', getRandomColor);
+    $('.spinning-dancer, .blinky-dancer').css({
+      'border-color': getRandomColor
+    });
+    $('.stick-dancer').css({
+      'background-color': getRandomColor
+    });
+  }, 1000);
 });
 
